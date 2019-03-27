b0737166f801e73b9e995260b9e4f178ff287509
Ari Mahardika A.N <arimahardika.an@gmail.com>
2019-03-27 22:25:45 +0700

    Add Moving Platform

---
+++ Assets/Scripts/AttachPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachPlayer : MonoBehaviour {
+
+	public GameObject Player;
+
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject == Player){
+			Player.transform.parent = transform;
+		}
+	}
+
+	/// <summary>
+	/// Sent when another object leaves a trigger collider attached to
+	/// this object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if(other.gameObject == Player){
+			Player.transform.parent = null;
+		}
+	}
+
+}
